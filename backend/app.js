9931353dd9b871a64daa2ce38091bdfab475fb0d
HotmangasiM <hotmangasi25@gmail.com>
2026-01-19 23:05:55 +0700

    first commit branch dev

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,29 @@
+require("dotenv").config();
+
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
+
 const sensorRoutes = require('./routes/sensor.routes.js');
 const startSheetScheduler = require('./jobs/sheetScheduler.js');
 
 const app = express();
+
 app.use(cors());
+app.use(express.json());
+
+//API ROUTES
 app.use('/api', sensorRoutes);
-const port = 3000;
+
+// serve frontend static
+app.use(express.static(path.join(__dirname, "public")));
 
 //START BABCKGROUND SCHEDULER
 startSheetScheduler();
 
-app.listen(port, () => {
-    console.log(`Server running on ${port}`);
+// PORT WAJIB seperti ini
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`);
 });
